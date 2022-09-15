97b79393f2aebfa4f57765e2013cc2433ff5adbf
Sean Narron <seannarron9@gmail.com>
2022-09-15 16:51:01 +0900

    Made a login page with joinRoom function.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import { useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
+
 
 function App() {
+
+  const [userName, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    socket.emit("join_room", room);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login">
+        <label htmlFor="">Name</label>
+        <input type="text" onChange={(e) => setUserName(e.target.value)}/><br />
+
+        <label htmlFor="">Room</label>
+        <input type="text" onChange={(e) => setRoom(e.target.value)}/>
+
+        <button onClick={joinRoom}>Join</button>
+      </div>
+      
+    
+      
     </div>
   );
 }
